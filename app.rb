7f98831ea040c844fe2b40915c88409245eb2dae
cocacola1 <danielhe108@icloud.com>
2025-01-21 10:16:28 +0100

    Update

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
-require 'becrypt'
+require 'bcrypt'
 
 get('/') do
     
-    
+    return slim(:start)
+end
+
+get('/login') do
+
+    return slim(:login)
 end
