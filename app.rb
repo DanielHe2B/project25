8c92963db0aed805081d53796cde0d6d9e3f1f04
cocacola1 <danielhe108@icloud.com>
2025-01-28 10:16:42 +0100

    Uppdatering
    Fortsatte med front end och börjar med back end

--- app.rb
+++ app.rb
@@ -4,12 +4,38 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 
+#enable :sessions
+
+@loggedin = true
+
 get('/') do
     
     return slim(:start)
 end
 
-get('/login') do
+get('/showlogin') do
 
     return slim(:login)
+end
+
+get('/register') do
+
+    return slim(:register)
+end
+
+post('/login') do
+    username=params[:username]
+    password=params[:password]
+    db=SQLite3::Database.new('db/slutprojekt.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?", username).first
+    passwordDigest = result["passwordDigest"]
+    id = result["id"]
+  
+    if BCrypt::Password.new(passwordDigest) == password
+      session[:id] = id
+      redirect('/')
+    else
+      "Fel lösenord"
+    end
 end
